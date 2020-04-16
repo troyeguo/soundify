c0301f0b136b48dc9611e2151e16915ec4268cb2
troyeguo <guo362429@163.com>
2020-04-16 10:01:36 +0800

    test

--- components/player.js
+++ components/player.js
@@ -232,7 +232,7 @@ class Player extends Component {
               This takes longer than expected, first make sure your internet
               connection to spotify is fine, then go to spotify and select
               Soundify manually in your spotify connect devices, refresh the
-              website after all work is done
+              website after all work is done.
             </div>
           </div>
         </div>
